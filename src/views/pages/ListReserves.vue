216e243d1c4370b9f5756458ccb04158655ee915
chrisr04 <christian971204@gmail.com>
2019-11-13 13:00:59 -0500

    delete reserve and facture

---
+++ src/views/pages/ListReserves.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Listado reservas</h1>
+        <Reserves></Reserves> 
+    </div>
+   
+</template>
+<script>
+import Reserves from "@/components/Reserves";
+export default {
+    name:"ListReserves",
+    components:{
+        Reserves
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
